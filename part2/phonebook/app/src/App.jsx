d76a5cf0fa7e6aa43a8f304fe2eab7fcd3946026
hdaer <hardar333@hotmail.com>
2025-01-30 16:30:23 +0100

    Update files; added db and json server

--- part2/phonebook/app/src/App.jsx
+++ part2/phonebook/app/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import initialPersons from "./data/persons.json";
+// import initialPersons from "./data/persons.json";
 
 import Filter from "./components/Filter";
 import NewPersonForm from "./components/NewPersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState(initialPersons);
+  const [persons, setPersons] = useState([]);
 
   const [newPerson, setNewPerson] = useState({ name: "", number: "", id: "" });
   const [nameFilter, setNameFilter] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    const getNumbers = async () => {
+      const respons = await axios.get("http://localhost:3001/persons");
+      const initialPersons = respons.data;
+      console.log("data received");
+      setPersons(initialPersons);
+    };
+    getNumbers();
+  }, []);
+
+  console.log("render", persons.length, "persons/numbers");
 
   const personsToDisplay = nameFilter
     ? persons.filter((person) => {
